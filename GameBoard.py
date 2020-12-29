6a64cf2ecede4e426fe371b92c98aa6185c8f273
Filbert-code <alexlingfungfilbert@gmail.com>
2020-12-29 10:45:21 -0800

    Added functions to Gameboard and added main.py

--- GameBoard.py
+++ GameBoard.py
@@ -6,22 +6,34 @@ class GameBoard:
     def __init__(self):
         self.board = self.createBoard()
 
+    # returns a 2D gameboard using Chars or Strings
     def createBoard(self):
-        # returns a 2D gameboard using Chars or Strings
         pass
 
+    # returns a list of tuples that indicate the (row, column)
+    # indices that will be changed when a player/computer makes
+    # a move
     def getMovePositions(self):
-        # returns a list of tuples that indicate the (row, column)
-        # indices that will be changed when a player/computer makes
-        # a move
         pass
 
+    # changes the Chars or Strings in move_positions to the values
+    # 0 - 8. These are the values the player will enter to indicate
+    # which position they'd like to play.
     def fillEmpty(self):
-        # changes the Chars or Strings in move_positions to the values
-        # 0 - 8. These are the values the player will enter to indicate
-        # which position they'd like to play.
         pass
 
+    # updates the 2D array with the new move
+    def playMove(self , pos, symbol):
+        pass
+
+    # updates the 2D array by removing the move at the given position
+    def undoMove(self , pos, symbol):
+        pass
+
+    # returns true if there are moves left to be play, false otherwise
+    def isMovesLeft(self):
+        pass
+
+    # print the state of the board
     def printBoard(self):
-        # print the state of the board
         pass
--- TicTacToe.py
+++ TicTacToe.py
@@ -1,3 +1,6 @@
+# This class contains the gameloop of the Tic-Tac-Toe game and uses its
+# findBestMove, minimax, and evaluation functions to find the best moves
+# for the computer to make. 
 from GameBoard import GameBoard
 import math
 
@@ -6,4 +9,35 @@ class TicTacToe:
     def __init(self):
         # instantiate a GameBoard object and create 1 or 2 arrays to
         # keep track of the state of the board
+        pass
+
+    # captures the user's choice, validates and updates the GameBoard.
+    def playerTurn(self):
+        pass
+
+    # the computer's turn to play a move
+    def compTurn(self):
+        pass
+
+    # prints to the command line the end-state of the game and exits the game
+    def winner(self):
+        pass
+
+    # returns the best move for the computer found using a backtracking
+    # algorithm called Minimax.
+    def findBestMove(self):
+        pass
+
+    # a recursive function that uses a backtracking algorithm to find the highest
+    # evaluation for the given gameboard object. The highest evaluation is
+    # returned.
+    def minimax(self):
+        pass
+
+    # checks if each player's chosen positions constitutes a win or not
+    def evaluate(self):
+        pass
+
+    # mainloop for the game
+    def startGame(self):
         pass
